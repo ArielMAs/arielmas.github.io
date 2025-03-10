55672bf947c6c6958b1b4d125225d890e112662b
Ariel <relruok@gmail.com>
2025-03-10 15:06:50 -0700

    job blog draft edits

--- src/job-market-gov/run.sh
+++ src/job-market-gov/run.sh
@@ -1,12 +1,16 @@
-# Source the .env file
-source secrets.env
+#!/bin/bash
 
-EMAIL=$SCRAPER_EMAIL
-AUTH_KEY=$SCRAPER_AUTH
+#Search Jobs API rate limitations
+#Maximum of 10,000 rows per query
+#Maximum of 500 rows per page
+
+EMAIL=$API_EMAIL
+AUTH_KEY=$API_AUTH
 BASE_URL="https://data.usajobs.gov/api/search"
 ROWS_PER_PAGE=500
 MAX_ROWS=10000
 PAGE=1
+
 echo running
 echo '[]' > main.json
 
@@ -20,8 +24,10 @@ while true; do
          "$BASE_URL?page=$PAGE&ResultsPerPage=$ROWS_PER_PAGE&DatePosted=60" \
          -o "page_$PAGE.json"
     
+    #Get number of results from current page .json
     RESULT_COUNT=$(jq '.SearchResult.SearchResultCount' "page_$PAGE.json")
 
+    #Select specific objects
     jq '[.SearchResult.SearchResultItems[].MatchedObjectDescriptor 
         | {PositionID, PositionTitle, PositionLocationDisplay, PositionLocation, OrganizationName, DepartmentName, JobCategory, QualificationSummary, PositionRemuneration, PublicationStartDate, UserArea: {Details: {JobSummary: .UserArea.Details.JobSummary, MajorDuties: .UserArea.Details.MajorDuties}}}]' \
         "page_$PAGE.json" > "tmp.json" && mv tmp.json "page_$PAGE.json"
@@ -29,15 +35,17 @@ while true; do
     # Merge the current page data into main.json
     jq -s 'add' main.json "page_$PAGE.json" > "tmp.json" && mv tmp.json main.json
     
-    #Clean
+    # Remove current page
     rm page_$PAGE.json
-    # Break if no more results or max rows reached
+    # Break if result has less than 500 rows
     if [ "$RESULT_COUNT" -lt $ROWS_PER_PAGE ]; then
         break
     fi
+
     echo "Page: "$PAGE
     echo "Result Count: "$RESULT_COUNT
     # Increment page number
     PAGE=$((PAGE + 1))
 done
+
 echo "Connection to API ended. Long JSON is ready."
